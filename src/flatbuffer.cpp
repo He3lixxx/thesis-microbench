58581876d4fcb8e5ea4a6ebe1c988a3f87b78910
Richard Ebeling <dev@richardebeling.de>
2022-01-20 15:02:33 +0100

    Clang format

--- src/flatbuffer.cpp
+++ src/flatbuffer.cpp
@@ -1,16 +1,18 @@
 #include <fmt/format.h>
-#include <cstdint>
 #include <array>
+#include <cstdint>
 
+#include "./tuple_generated.h"
 #include "bench.hpp"
 #include "flatbuffer.hpp"
-#include "./tuple_generated.h"
 
 IMPL_VISIBILITY void serialize_flatbuffer(const NativeTuple& tup, fmt::memory_buffer* buf) {
     thread_local flatbuffers::FlatBufferBuilder builder(1024);
-    auto hash_bytes_span = flatbuffers::make_span(reinterpret_cast<const std::array<uint8_t, HASH_BYTES>&>(tup.container_id));
+    auto hash_bytes_span = flatbuffers::make_span(
+        reinterpret_cast<const std::array<uint8_t, HASH_BYTES>&>(tup.container_id));
     auto h = Hash(hash_bytes_span);
-    CreateTuple(builder, tup.id, tup.timestamp, tup.load, tup.load_avg_1, tup.load_avg_5, tup.load_avg_15, &h);
+    CreateTuple(builder, tup.id, tup.timestamp, tup.load, tup.load_avg_1, tup.load_avg_5,
+                tup.load_avg_15, &h);
 
     const size_t size = builder.GetSize();
     buf->reserve(buf->size() + size + sizeof(size));
@@ -20,19 +22,21 @@ IMPL_VISIBILITY void serialize_flatbuffer(const NativeTuple& tup, fmt::memory_bu
 }
 
 IMPL_VISIBILITY size_t parse_flatbuffer(const std::byte* __restrict__ read_ptr, NativeTuple* tup) {
-    // TODO: This copies all elements, and doesn't use the advantage that direct attribute access is possible
+    // TODO: This copies all elements, and doesn't use the advantage that direct attribute access is
+    // possible
     size_t size = 0;
     std::copy_n(read_ptr, sizeof(size), reinterpret_cast<std::byte*>(&size));
     read_ptr += sizeof(size);
 
-    const auto *t = GetTuple(read_ptr);
+    const auto* t = GetTuple(read_ptr);
     tup->id = t->id();
     tup->timestamp = t->timestamp();
     tup->load = t->load();
     tup->load = t->load_avg_1();
     tup->load = t->load_avg_5();
     tup->load = t->load_avg_15();
-    std::copy_n(t->container_id()->bytes()->data(), HASH_BYTES, reinterpret_cast<uint8_t*>(tup->container_id.data()));
+    std::copy_n(t->container_id()->bytes()->data(), HASH_BYTES,
+                reinterpret_cast<uint8_t*>(tup->container_id.data()));
 
     return size + sizeof(size);
 }
