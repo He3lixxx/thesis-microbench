c5f5c1018f4d76fe1b6213568a5dff69c0b6b15b
Richard Ebeling <dev@richardebeling.de>
2022-08-22 23:51:51 +0200

    Only serialize and parse int fields

--- src/flatbuffer.cpp
+++ src/flatbuffer.cpp
@@ -8,11 +8,7 @@
 
 IMPL_VISIBILITY void serialize_flatbuffer(const NativeTuple& tup, std::vector<std::byte>* buf) {
     thread_local flatbuffers::FlatBufferBuilder builder(1024);
-    auto hash_bytes_span = flatbuffers::make_span(
-        reinterpret_cast<const std::array<uint8_t, HASH_BYTES>&>(tup.container_id));
-    auto h = Hash(hash_bytes_span);
-    auto tuple_offset = CreateTuple(builder, tup.id, tup.timestamp, tup.load, tup.load_avg_1,
-                                    tup.load_avg_5, tup.load_avg_15, &h);
+    auto tuple_offset = CreateTuple(builder, tup.id, tup.timestamp);
     builder.Finish(tuple_offset);
 
     const size_t size = builder.GetSize();
@@ -37,12 +33,6 @@ IMPL_VISIBILITY bool parse_flatbuffer(const std::byte* __restrict__ read_ptr,
     const auto* t = GetTuple(read_ptr);
     tup->id = t->id();
     tup->timestamp = t->timestamp();
-    tup->load = t->load();
-    tup->load_avg_1 = t->load_avg_1();
-    tup->load_avg_5 = t->load_avg_5();
-    tup->load_avg_15 = t->load_avg_15();
-    std::copy_n(t->container_id()->bytes()->data(), HASH_BYTES,
-                reinterpret_cast<uint8_t*>(tup->container_id.data()));
 
     return true;
 }
